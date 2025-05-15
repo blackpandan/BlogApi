59dc5e3faa16f3c15c40f08156a13365b8bb1131
blackpandan <faithgstc54@gmail.com>
2025-05-15 12:26:08 +0100

    NEW: added new Tests project

---
+++ Tests/UnitTests/BlogTests.cs
@@ -0,0 +1,16 @@
+using Xunit;
+using Moq;
+using BlogApi.Core.Entities;
+using BlogApi.Infrastructure.Repositories;
+
+
+namespace Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
