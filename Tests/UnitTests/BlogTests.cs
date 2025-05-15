29ad2bcc5ba117d2aef4295416b870e50e5f04cf
blackpandan <faithgstc54@gmail.com>
2025-05-15 02:27:42 +0100

    NEW: added unit test to verify BlogByIdAsync in BlogRespository returns correct blog

--- Tests/UnitTests/BlogTests.cs
+++ Tests/UnitTests/BlogTests.cs
@@ -1,16 +1,50 @@
-using Xunit;
-using Moq;
 using BlogApi.Core.Entities;
+using BlogApi.Core.Interfaces;
 using BlogApi.Infrastructure.Repositories;
+using BlogApi.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
 
 
-namespace Tests;
-
-public class UnitTest1
+namespace BlogApi.Tests.UnitTests
 {
-    [Fact]
-    public void Test1()
+    public class BlogRepositoryTests
     {
-        Assert.True(true);
+        private readonly BlogApiContext _context;
+        private readonly BlogRepository _repository;
+
+        public BlogRepositoryTests()
+        {
+            var options = new DbContextOptionsBuilder<BlogApiContext>()
+                .UseInMemoryDatabase(databaseName: "TestDB")
+                .Options;
+
+            _context = new BlogApiContext(options);
+            _repository = new BlogRepository(_context);
+        }
+
+        [Fact]
+        public async Task GetBlogByIdAsync_ReturnsCorrectBlog()
+        {
+            // Arrange
+            var author = new Author ("Faith", "test@gmail.com");
+            _context.Authors.Add(author);
+            await _context.SaveChangesAsync(TestContext.Current.CancellationToken);
+
+            var blog = new Blog(new Uri("http://localhost:5000/Test-Blog"),
+                "Test Blog",
+                author
+            );
+            _context.Blogs.Add(blog);
+            await _context.SaveChangesAsync(TestContext.Current.CancellationToken);
+             
+            // Act
+            var result = await _repository.GetByIdAsync(blog.Id);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal("Test Blog", result.Title);
+            Assert.Equal(author.Id, result.AuthorId);
+        }
     }
 }
