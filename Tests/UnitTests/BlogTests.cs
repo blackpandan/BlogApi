20030f3b182ffc534497ff4813982beccffe8b63
blackpandan <faithgstc54@gmail.com>
2025-05-15 05:46:58 +0100

    Edit: replaced context and used moq and also  Query and Handler for Blog

--- Tests/UnitTests/BlogTests.cs
+++ Tests/UnitTests/BlogTests.cs
@@ -4,42 +4,47 @@ using BlogApi.Infrastructure.Repositories;
 using BlogApi.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Xunit;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using BlogApi.Application.Handlers.BlogHandler;
+using BlogApi.Application.Queries.BlogQueries;
 
 
 namespace BlogApi.Tests.UnitTests
 {
     public class BlogRepositoryTests
     {
-        private readonly BlogApiContext _context;
-        private readonly BlogRepository _repository;
-
-        public BlogRepositoryTests()
-        {
-            var options = new DbContextOptionsBuilder<BlogApiContext>()
-                .UseInMemoryDatabase(databaseName: "TestDB")
-                .Options;
-
-            _context = new BlogApiContext(options);
-            _repository = new BlogRepository(_context);
-        }
 
         [Fact]
         public async Task GetBlogByIdAsync_ReturnsCorrectBlog()
         {
             // Arrange
-            var author = new Author ("Faith", "test@gmail.com");
-            _context.Authors.Add(author);
-            await _context.SaveChangesAsync(TestContext.Current.CancellationToken);
+            var mockRepo = new Mock<IBlogRepository>();
 
-            var blog = new Blog(new Uri("http://localhost:5000/Test-Blog"),
+            int blogId = 1;
+            var author = new Author("Faith", "test@gmail.com");
+            var expectedBlog = new Blog(new Uri("http://localhost:5000/Test-Blog"),
                 "Test Blog",
                 author
             );
-            _context.Blogs.Add(blog);
-            await _context.SaveChangesAsync(TestContext.Current.CancellationToken);
-             
+
+            mockRepo.Setup(repo => repo.GetByIdAsync(blogId)).ReturnsAsync(expectedBlog);
+
+            var handler = new GetBlogByIdHandler(mockRepo.Object);
+            var query = new GetBlogByIdQuery(blogId);
+            // _context.Authors.Add(author);
+            // await _context.SaveChangesAsync(TestContext.Current.CancellationToken);
+
+            // var blog = new Blog(new Uri("http://localhost:5000/Test-Blog"),
+            //     "Test Blog",
+            //     author
+            // );
+            // _context.Blogs.Add(blog);
+            // await _context.SaveChangesAsync(TestContext.Current.CancellationToken);
+
             // Act
-            var result = await _repository.GetByIdAsync(blog.Id);
+            var result = await handler.HandleAsync(query);
 
             // Assert
             Assert.NotNull(result);
