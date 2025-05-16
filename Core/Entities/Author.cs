d41d1921f2d9ddbd50d29f923cb63a2d64892da4
blackpandan <faithgstc54@gmail.com>
2025-05-16 19:08:24 +0100

    NEW: ADDED AUTHOR ENTITY AT CORE/ENTITIES

---
+++ Core/Entities/Author.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogApi.Core.Entities
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public EmailAddressAttribute Email { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        public virtual ICollection<Blog> Blogs { get; private set; }
+
+        public Author(string name, EmailAddressAttribute email)
+        {
+            Name = name;
+            Email = email;
+            Blogs = [];
+        }
+     }
+}
